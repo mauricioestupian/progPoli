85f5248c00ae3715a3998ade9a114a228730c457
Mauricio Estupiñan <mauricio_estupinan@yahoo.com>
2025-05-08 09:44:47 -0500

    primer commit

---
+++ src/main/java/com/popli/programacion/ProgramacionApplication.java
@@ -0,0 +1,13 @@
+package com.popli.programacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popli/programacion/ProgramacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popli.programacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
